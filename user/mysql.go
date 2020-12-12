f6c23d0671b21d545d5fab06fbb199cf84891338
Hairizuan <hairizuanbinnoorazman@gmail.com>
2020-12-12 21:26:33 +0800

    added userstore mysql instantiation

--- user/mysql.go
+++ user/mysql.go
@@ -3,11 +3,20 @@ package user
 import (
 	"context"
 
+	"github.com/hairizuanbinnoorazman/slides-to-video-manager/logger"
 	"github.com/jinzhu/gorm"
 )
 
 type mysql struct {
-	db *gorm.DB
+	db     *gorm.DB
+	logger logger.Logger
+}
+
+func NewMySQL(logger logger.Logger, dbClient *gorm.DB) mysql {
+	return mysql{
+		db:     dbClient,
+		logger: logger,
+	}
 }
 
 func (m mysql) StoreUser(ctx context.Context, u User) error {
