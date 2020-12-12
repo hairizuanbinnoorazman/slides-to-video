80e7bdc28b86523dd78621c1f129cc75e970abe0
Hairizuan <hairizuanbinnoorazman@gmail.com>
2020-12-13 01:19:27 +0800

    added the get, getall, update and delete functionalities

--- pdfslideimages/mysql.go
+++ pdfslideimages/mysql.go
@@ -28,17 +28,46 @@ func (m mysql) Create(ctx context.Context, e PDFSlideImages) error {
 }
 
 func (m mysql) Get(ctx context.Context, projectID, ID string) (PDFSlideImages, error) {
-	return PDFSlideImages{}, nil
+	p := PDFSlideImages{}
+	result := m.db.Where("id = ? AND project_id = ?", ID, projectID).First(&p)
+	if result.Error != nil {
+		return p, result.Error
+	}
+	return p, nil
 }
 
 func (m mysql) GetAll(ctx context.Context, ProjectID string, Limit, After int) ([]PDFSlideImages, error) {
-	return []PDFSlideImages{}, nil
+	var pdfSlideImages []PDFSlideImages
+	result := m.db.Where("project_id = ?", ProjectID).Limit(Limit).Offset(After).Find(&pdfSlideImages)
+	if result.Error != nil {
+		return []PDFSlideImages{}, result.Error
+	}
+	return pdfSlideImages, nil
 }
 
 func (m mysql) Update(ctx context.Context, projectID, ID string, setters ...func(*PDFSlideImages) error) (PDFSlideImages, error) {
-	return PDFSlideImages{}, nil
+	var p PDFSlideImages
+	result := m.db.Where("project_id = ? AND id = ?", projectID, ID).First(&p)
+	if result.Error != nil {
+		return PDFSlideImages{}, result.Error
+	}
+	for _, s := range setters {
+		err := s(&p)
+		if err != nil {
+			return PDFSlideImages{}, err
+		}
+	}
+	result = m.db.Save(&p)
+	if result.Error != nil {
+		return PDFSlideImages{}, result.Error
+	}
+	return p, nil
 }
 
 func (m mysql) Delete(ctx context.Context, projectID, ID string) error {
+	result := m.db.Where("id = ? and project_id = ?", ID, projectID).Delete(PDFSlideImages{})
+	if result.Error != nil {
+		return result.Error
+	}
 	return nil
 }
