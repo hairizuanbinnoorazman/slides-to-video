12d81093ecc3c6fa1e4d35f0187897c628f85d23
Hairizuan <hairizuanbinnoorazman@gmail.com>
2020-12-12 21:35:10 +0800

    fixed: Added mysql usage for videosegment data

---
+++ videosegment/mysql.go
@@ -0,0 +1,40 @@
+package videosegment
+
+import (
+	"context"
+
+	"github.com/hairizuanbinnoorazman/slides-to-video-manager/logger"
+	"github.com/jinzhu/gorm"
+)
+
+type mysql struct {
+	db     *gorm.DB
+	logger logger.Logger
+}
+
+func NewMySQL(logger logger.Logger, dbClient *gorm.DB) mysql {
+	return mysql{
+		db:     dbClient,
+		logger: logger,
+	}
+}
+
+func (m mysql) Create(ctx context.Context, e VideoSegment) error {
+	return nil
+}
+
+func (m mysql) Get(ctx context.Context, projectID, ID string) (VideoSegment, error) {
+	return VideoSegment{}, nil
+}
+
+func (m mysql) GetAll(ctx context.Context, ProjectID string, Limit, After int) ([]VideoSegment, error) {
+	return []VideoSegment{}, nil
+}
+
+func (m mysql) Update(ctx context.Context, projectID, ID string, setters ...func(*VideoSegment) error) (VideoSegment, error) {
+	return VideoSegment{}, nil
+}
+
+func (m mysql) Delete(ctx context.Context, projectID, ID string) error {
+	return nil
+}
