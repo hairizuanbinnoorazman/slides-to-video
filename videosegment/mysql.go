29311c9e16078f80ad242ae838e48a7341435596
Hairizuan <hairizuanbinnoorazman@gmail.com>
2020-12-13 02:49:26 +0800

    added initial version of crud operations for videosegment

--- videosegment/mysql.go
+++ videosegment/mysql.go
@@ -20,21 +20,54 @@ func NewMySQL(logger logger.Logger, dbClient *gorm.DB) mysql {
 }
 
 func (m mysql) Create(ctx context.Context, e VideoSegment) error {
+	result := m.db.Create(&e)
+	if result.Error != nil {
+		return result.Error
+	}
 	return nil
 }
 
 func (m mysql) Get(ctx context.Context, projectID, ID string) (VideoSegment, error) {
-	return VideoSegment{}, nil
+	p := VideoSegment{}
+	result := m.db.Where("id = ? AND project_id = ?", ID, projectID).First(&p)
+	if result.Error != nil {
+		return p, result.Error
+	}
+	return p, nil
 }
 
 func (m mysql) GetAll(ctx context.Context, ProjectID string, Limit, After int) ([]VideoSegment, error) {
-	return []VideoSegment{}, nil
+	var videosegments []VideoSegment
+	result := m.db.Where("project_id = ?", ProjectID).Limit(Limit).Offset(After).Find(&videosegments)
+	if result.Error != nil {
+		return []VideoSegment{}, result.Error
+	}
+	return videosegments, nil
 }
 
 func (m mysql) Update(ctx context.Context, projectID, ID string, setters ...func(*VideoSegment) error) (VideoSegment, error) {
-	return VideoSegment{}, nil
+	var p VideoSegment
+	result := m.db.Where("project_id = ? AND id = ?", projectID, ID).First(&p)
+	if result.Error != nil {
+		return VideoSegment{}, result.Error
+	}
+	for _, s := range setters {
+		err := s(&p)
+		if err != nil {
+			return VideoSegment{}, err
+		}
+	}
+	result = m.db.Save(&p)
+	if result.Error != nil {
+		return VideoSegment{}, result.Error
+	}
+	return p, nil
 }
 
 func (m mysql) Delete(ctx context.Context, projectID, ID string) error {
+	result := m.db.Where("id = ? and project_id = ?", ID, projectID).Delete(VideoSegment{})
+	if result.Error != nil {
+		return result.Error
+	}
 	return nil
 }
