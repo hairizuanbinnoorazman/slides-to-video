33fe69167e549cb38b73e075be0ed41a51de25f0
Hairizuan <hairizuanbinnoorazman@gmail.com>
2020-12-14 01:06:39 +0800

    added blobstorage abstraction as well as http/queue mode variant

--- cmd/pdf-splitter/serve.go
+++ cmd/pdf-splitter/serve.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 
+	"github.com/hairizuanbinnoorazman/slides-to-video-manager/cmd/pdf-splitter/mgrclient"
+
+	"cloud.google.com/go/storage"
+	"github.com/hairizuanbinnoorazman/slides-to-video-manager/blobstorage"
 	h "github.com/hairizuanbinnoorazman/slides-to-video-manager/cmd/pdf-splitter/handlers"
+	"google.golang.org/api/option"
 
 	stackdriver "github.com/TV4/logrus-stackdriver-formatter"
 	"github.com/gorilla/mux"
@@ -38,10 +45,49 @@ var (
 					os.Exit(1)
 				}
 
+				var svcAcctOptions []option.ClientOption
+				if cfg.Server.SvcAcctFile != "" {
+					credJSON, err := ioutil.ReadFile(cfg.Server.SvcAcctFile)
+					if err != nil {
+						logger.Errorf("Unable to load slides-to-video-manager cred file. err: %v", err)
+					}
+					svcAcctOptions = append(svcAcctOptions, option.WithCredentialsJSON(credJSON))
+				}
+
+				var slideToVideoStorage blobstorage.BlobStorage
+				if cfg.BlobStorage.Type == gcsBlobStorage {
+					var xClient *storage.Client
+					xClient, err = storage.NewClient(context.Background(), svcAcctOptions...)
+					if err != nil {
+						logger.Errorf("Unable to create storage client %v", err)
+						os.Exit(1)
+					}
+					slideToVideoStorage = blobstorage.NewGCSStorage(logger, xClient, cfg.BlobStorage.GCS.Bucket)
+				} else if cfg.BlobStorage.Type == minioBlobStorage {
+					slideToVideoStorage, err = blobstorage.NewMinio(logger, cfg.BlobStorage.Minio.Endpoint, cfg.BlobStorage.Minio.AccessKeyID, cfg.BlobStorage.Minio.SecretAccessKey, cfg.BlobStorage.Minio.Bucket)
+					if err != nil {
+						logger.Errorf("Unable to create storage client %v", err)
+						os.Exit(1)
+					}
+				}
+
+				if slideToVideoStorage == nil {
+					logger.Errorf("Some of the storage instantiation is nil")
+					os.Exit(1)
+				}
+
 				r := mux.NewRouter()
 				r.Handle("/status", h.Status{
 					Logger: logger,
 				})
+
+				if cfg.Server.Mode == "http" {
+					r.Handle(cfg.Server.ProcessRoute, h.ProcessHandler{
+						Logger:               logger,
+						SlidesToVideoStorage: slideToVideoStorage,
+						MgrClient:            mgrclient.NewBasic("", logger, http.DefaultClient),
+					})
+				}
 
 				srv := http.Server{
 					Addr: fmt.Sprintf("%v:%v", cfg.Server.Host, cfg.Server.Port),
