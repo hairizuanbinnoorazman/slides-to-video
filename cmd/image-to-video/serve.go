71a65c4382145988bf52292fa41ef8be350c742e
Hairizuan <hairizuanbinnoorazman@gmail.com>
2020-12-16 04:07:17 +0800

    added initial set of code to instantiate blob storage etc

--- cmd/image-to-video/serve.go
+++ cmd/image-to-video/serve.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 
+	"cloud.google.com/go/storage"
+	"github.com/hairizuanbinnoorazman/slides-to-video-manager/blobstorage"
 	h "github.com/hairizuanbinnoorazman/slides-to-video-manager/cmd/image-to-video/handlers"
+	"github.com/hairizuanbinnoorazman/slides-to-video-manager/cmd/image-to-video/mgrclient"
+	"google.golang.org/api/option"
 
 	stackdriver "github.com/TV4/logrus-stackdriver-formatter"
 	"github.com/gorilla/mux"
@@ -37,6 +43,39 @@ var (
 					logger.Errorf("Error with loading configuration. %v", err)
 					os.Exit(1)
 				}
+
+				var svcAcctOptions []option.ClientOption
+				if cfg.Server.SvcAcctFile != "" {
+					credJSON, err := ioutil.ReadFile(cfg.Server.SvcAcctFile)
+					if err != nil {
+						logger.Errorf("Unable to load slides-to-video-manager cred file. err: %v", err)
+					}
+					svcAcctOptions = append(svcAcctOptions, option.WithCredentialsJSON(credJSON))
+				}
+
+				var slideToVideoStorage blobstorage.BlobStorage
+				if cfg.BlobStorage.Type == gcsBlobStorage {
+					var xClient *storage.Client
+					xClient, err = storage.NewClient(context.Background(), svcAcctOptions...)
+					if err != nil {
+						logger.Errorf("Unable to create storage client %v", err)
+						os.Exit(1)
+					}
+					slideToVideoStorage = blobstorage.NewGCSStorage(logger, xClient, cfg.BlobStorage.GCS.Bucket)
+				} else if cfg.BlobStorage.Type == minioBlobStorage {
+					slideToVideoStorage, err = blobstorage.NewMinio(logger, cfg.BlobStorage.Minio.Endpoint, cfg.BlobStorage.Minio.AccessKeyID, cfg.BlobStorage.Minio.SecretAccessKey, cfg.BlobStorage.Minio.Bucket)
+					if err != nil {
+						logger.Errorf("Unable to create storage client %v", err)
+						os.Exit(1)
+					}
+				}
+
+				if slideToVideoStorage == nil {
+					logger.Errorf("Some of the storage instantiation is nil")
+					os.Exit(1)
+				}
+
+				_ = mgrclient.NewBasic(logger, fmt.Sprintf("http://%v:%v/api/v1", cfg.Server.ManagerHost, cfg.Server.ManagerPort), http.DefaultClient)
 
 				r := mux.NewRouter()
 				r.Handle("/status", h.Status{
