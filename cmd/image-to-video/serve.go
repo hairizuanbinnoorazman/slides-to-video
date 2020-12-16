86499f741a27952917adc107a03333273352c514
Hairizuan <hairizuanbinnoorazman@gmail.com>
2020-12-17 03:39:47 +0800

    complete integration of image to video serve

--- cmd/image-to-video/serve.go
+++ cmd/image-to-video/serve.go
@@ -7,7 +7,13 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/hairizuanbinnoorazman/slides-to-video-manager/cmd/image-to-video/image2videoconverter"
+	"github.com/hairizuanbinnoorazman/slides-to-video-manager/cmd/image-to-video/queuehandler"
+	"github.com/hairizuanbinnoorazman/slides-to-video-manager/queue"
+
+	"cloud.google.com/go/pubsub"
 	"cloud.google.com/go/storage"
+	texttospeech "cloud.google.com/go/texttospeech/apiv1"
 	"github.com/hairizuanbinnoorazman/slides-to-video-manager/blobstorage"
 	h "github.com/hairizuanbinnoorazman/slides-to-video-manager/cmd/image-to-video/handlers"
 	"github.com/hairizuanbinnoorazman/slides-to-video-manager/cmd/image-to-video/mgrclient"
@@ -44,9 +50,10 @@ var (
 					os.Exit(1)
 				}
 
+				var credJSON []byte
 				var svcAcctOptions []option.ClientOption
 				if cfg.Server.SvcAcctFile != "" {
-					credJSON, err := ioutil.ReadFile(cfg.Server.SvcAcctFile)
+					credJSON, err = ioutil.ReadFile(cfg.Server.SvcAcctFile)
 					if err != nil {
 						logger.Errorf("Unable to load slides-to-video-manager cred file. err: %v", err)
 					}
@@ -75,12 +82,52 @@ var (
 					os.Exit(1)
 				}
 
-				_ = mgrclient.NewBasic(logger, fmt.Sprintf("http://%v:%v/api/v1", cfg.Server.ManagerHost, cfg.Server.ManagerPort), http.DefaultClient)
+				text2speechClient, err := texttospeech.NewClient(context.Background(), option.WithCredentialsJSON(credJSON))
+				if err != nil {
+					logger.Error("Unable to create text to speech client")
+				}
+
+				mgrclient := mgrclient.NewBasic(logger, fmt.Sprintf("http://%v:%v/api/v1", cfg.Server.ManagerHost, cfg.Server.ManagerPort), http.DefaultClient)
+				textToSpeechEngine := image2videoconverter.NewGoogleTextToSpeech(logger, text2speechClient)
+				image2videoConverter := image2videoconverter.NewBasic(logger, slideToVideoStorage, mgrclient, cfg.BlobStorage.ImagesFolder, cfg.BlobStorage.VideoSnippetsFolder, &textToSpeechEngine)
 
 				r := mux.NewRouter()
 				r.Handle("/status", h.Status{
 					Logger: logger,
 				})
+
+				if cfg.Server.Mode == "http" {
+					r.Handle(cfg.Server.ProcessRoute, h.ProcessHandler{
+						Logger:               logger,
+						Image2VideoConverter: &image2videoConverter,
+					})
+				}
+
+				if cfg.Server.Mode == "queue" {
+					var imageToVideoQueue queue.Queue
+					if cfg.Queue.Type == googlePubsubQueue {
+						pubsubClient, err := pubsub.NewClient(context.Background(), cfg.Queue.GooglePubsub.ProjectID, svcAcctOptions...)
+						if err != nil {
+							logger.Errorf("Unable to create pubsub client. %v", err)
+							os.Exit(1)
+						}
+
+						imageToVideoQueue = queue.NewGooglePubsub(logger, pubsubClient, cfg.Queue.ImageToVideoTopic)
+					} else if cfg.Queue.Type == natsQueue {
+						imageToVideoQueue, err = queue.NewNats(logger, cfg.Queue.NatsConfig.Endpoint, cfg.Queue.ImageToVideoTopic)
+						if err != nil {
+							logger.Errorf("Unable to create Nats client. %v", err)
+						}
+					}
+
+					if imageToVideoQueue == nil {
+						logger.Errorf("Some of the queue instantiation is nil")
+						os.Exit(1)
+					}
+
+					queueHandler := queuehandler.NewBasic(logger, imageToVideoQueue, &image2videoConverter)
+					go queueHandler.HandleMessages()
+				}
 
 				srv := http.Server{
 					Addr: fmt.Sprintf("%v:%v", cfg.Server.Host, cfg.Server.Port),
