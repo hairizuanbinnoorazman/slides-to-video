7795bd077d9dea28c8e89f372f93567df9ed1d07
Hairizuan <hairizuanbinnoorazman@gmail.com>
2021-02-13 05:44:52 +0800

    added mysql db interface

--- user/mysql.go
+++ user/mysql.go
@@ -20,13 +20,27 @@ func NewMySQL(logger logger.Logger, dbClient *gorm.DB) mysql {
 }
 
 func (m mysql) StoreUser(ctx context.Context, u User) error {
+	result := m.db.Create(&u)
+	if result.Error != nil {
+		return result.Error
+	}
 	return nil
 }
 
 func (m mysql) GetUser(ctx context.Context, ID string) (User, error) {
-	return User{}, nil
+	u := User{}
+	result := m.db.Where("id = ?", ID).First(&u)
+	if result.Error != nil {
+		return User{}, result.Error
+	}
+	return u, nil
 }
 
 func (m mysql) GetUserByEmail(ctx context.Context, Email string) (User, error) {
-	return User{}, nil
+	u := User{}
+	result := m.db.Where("email = ?", Email).First(&u)
+	if result.Error != nil {
+		return User{}, result.Error
+	}
+	return u, nil
 }
