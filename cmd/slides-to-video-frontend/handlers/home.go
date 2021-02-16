273b06512329a42f414b1efd8e5ddb61f285f276
Hairizuan <hairizuanbinnoorazman@gmail.com>
2021-02-17 03:00:48 +0800

    alter home page to be dashboard page instead

--- cmd/slides-to-video-frontend/handlers/home.go
+++ cmd/slides-to-video-frontend/handlers/home.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"encoding/json"
+	"html/template"
 	"net/http"
 
 	"github.com/hairizuanbinnoorazman/slides-to-video-manager/logger"
@@ -15,11 +15,14 @@ func (h Home) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	h.Logger.Info("Start Home Handler")
 	defer h.Logger.Info("End Home Handler")
 
-	type resp struct {
-		Status string `json:"status"`
+	tmpl, err := template.ParseFiles("./templates/dashboard.html", "./templates/head.html", "./templates/header.html")
+	if err != nil {
+		h.Logger.Info("Error: %v", err)
+		w.Write([]byte("Failed to render login page"))
+		return
 	}
-	raw, _ := json.Marshal(resp{Status: "ok"})
+	err = tmpl.Execute(w, nil)
+
 	w.WriteHeader(http.StatusOK)
-	w.Write(raw)
 	return
 }
