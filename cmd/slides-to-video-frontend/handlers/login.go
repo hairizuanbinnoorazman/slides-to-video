a90b3cbc699348c5f4d42095d0a46a8bd5006e22
Hairizuan <hairizuanbinnoorazman@gmail.com>
2021-02-17 02:56:35 +0800

    alter the login page to have bootstrap css and js added to it

--- cmd/slides-to-video-frontend/handlers/login.go
+++ cmd/slides-to-video-frontend/handlers/login.go
@@ -18,7 +18,7 @@ func (h Login) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	h.Logger.Info("Start Login Handler")
 	defer h.Logger.Info("End Login Handler")
 
-	tmpl, err := template.ParseFiles("./templates/login.html")
+	tmpl, err := template.ParseFiles("./templates/login.html", "./templates/head.html", "./templates/header.html")
 	if err != nil {
 		h.Logger.Info("Error: %v", err)
 		w.Write([]byte("Failed to render login page"))
