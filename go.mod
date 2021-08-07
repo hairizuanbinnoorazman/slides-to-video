b36c5d4521d842546cd1f50cb4c2fae5ce87538d
Hairizuan <hairizuanbinnoorazman@gmail.com>
2021-08-08 05:37:01 +0800

    feat: Adding capability to have user with password logins

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/TV4/logrus-stackdriver-formatter v0.1.0
 	github.com/gofrs/uuid v3.2.0+incompatible
 	github.com/golang-jwt/jwt/v4 v4.0.0
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
 	github.com/imdario/mergo v0.3.10
@@ -20,6 +21,7 @@ require (
 	github.com/sirupsen/logrus v1.7.0
 	github.com/spf13/cobra v1.0.0
 	github.com/testcontainers/testcontainers-go v0.9.0
+	golang.org/x/crypto v0.0.0-20210314154223-e6e6c4f2bb5b // indirect
 	google.golang.org/api v0.9.0
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
 	google.golang.org/protobuf v1.25.0 // indirect
--- user/user.go
+++ user/user.go
@@ -3,22 +3,148 @@ package user
 
 import (
 	"context"
+	"errors"
+	"regexp"
 	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type Type string
+
+var PasswordAuth Type = "password"
+var EmailAuth Type = "passwordless-email"
+var GoogleAuth Type = "google"
+
+var ErrEmailInvalid = errors.New("Email is invalid")
+var ErrPasswordShort = errors.New("Password cannot be shorter than 8 characters")
+var ErrPasswordLong = errors.New("Password cannot be longer than 120 characters")
+var ErrPasswordInvalid = errors.New("Password requires at least 1 capital letter, 1 small letter and a number")
+var ErrSamePassword = errors.New("Current password already in use. Please pick another password")
+var ErrActivationTokenInvalid = errors.New("Activation Token is invalid")
+var ErrForgetPasswordTokenInvalid = errors.New("Forget Password Token is invalid")
 
 // User represents a single user - current assumes that user would be one that a google account
 type User struct {
-	ID           string
-	Email        string
-	RefreshToken string
-	AuthToken    string
-	Type         string
+	ID                       string    `gorm:"type:varchar(40);primary_key"`
+	Email                    string    `gorm:"type:varchar(250)"`
+	Password                 string    `json:"-" gorm:"type:varchar(250)"`
+	ForgetPasswordToken      string    `json:"-" gorm:"type:varchar(40)"`
+	ForgetPasswordExpiryDate time.Time `json:"-"`
+	ActivationToken          string    `json:"-" gorm:"type:varchar(40)"`
+	ActivationExpiryDate     time.Time `json:"-"`
+	Activated                bool      `json:"-"`
+	// Google Auth Tokens
+	RefreshToken string `gorm:"type:varchar(250)"`
+	AuthToken    string `gorm:"type:varchar(250)"`
+	Type         string `gorm:"type:varchar(250)"`
 	DateCreated  time.Time
 	DateModified time.Time
+}
+
+func NewUser(firstName, lastName, email, password string) (*User, error) {
+	user := User{Email: email}
+	err := user.setPassword(password)
+	user.ID = uuid.New().String()
+	user.ActivationToken = uuid.New().String()
+	user.DateCreated = time.Now()
+	user.DateModified = time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
+	user.ForgetPasswordExpiryDate = time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
+	user.ActivationExpiryDate = time.Now().Add(1 * time.Hour)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
 
 type Store interface {
 	StoreUser(ctx context.Context, u User) error
 	GetUser(ctx context.Context, ID string) (User, error)
 	GetUserByEmail(ctx context.Context, Email string) (User, error)
+}
+
+func (u User) validateEmail() error {
+	reEmail := regexp.MustCompile(`\w+@\w{2,3}.\w{2,3}`)
+	isValid := reEmail.MatchString(u.Email)
+	if isValid {
+		return nil
+	}
+	return ErrEmailInvalid
+}
+
+func (u *User) setPassword(password string) error {
+	if len(password) < 8 {
+		return ErrPasswordShort
+	}
+	if len(password) > 120 {
+		return ErrPasswordLong
+	}
+	reSmallLetters := regexp.MustCompile("[a-z]")
+	reCapital := regexp.MustCompile("[A-Z]")
+	reNumbers := regexp.MustCompile("[0-9]")
+	smallLettersFind := reSmallLetters.FindAllString(password, -1)
+	capitalFind := reCapital.FindAllString(password, -1)
+	numberFind := reNumbers.FindAllString(password, -1)
+	if len(smallLettersFind) > 0 && len(capitalFind) > 0 && len(numberFind) > 0 {
+		hashedPassword, errEncrpt := bcrypt.GenerateFromPassword([]byte(password), 10)
+		if errEncrpt != nil {
+			return ErrPasswordInvalid
+		}
+		u.Password = string(hashedPassword)
+		return nil
+	}
+	return ErrPasswordInvalid
+}
+
+// ForgetPassword resets the forget password token to a random UUID as well as resets the
+// forget password expiry token. The function will return the forgetPasswordToken
+func (u *User) ForgetPassword() (string, error) {
+	u.ForgetPasswordToken = uuid.New().String()
+	u.ForgetPasswordExpiryDate = time.Now()
+	return u.ForgetPasswordToken, nil
+}
+
+// ChangePasswordFromForget requires you to provide the forget password token. This function
+// will then check the forgetPasswordToken if its correct and alters it accordingly
+func (u *User) ChangePasswordFromForget(forgetPasswordToken, password string) error {
+	if u.ForgetPasswordToken == forgetPasswordToken {
+		err := u.setPassword(password)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	return ErrForgetPasswordTokenInvalid
+}
+
+// ChangePassword changes the password on the user object before saving it
+func (u *User) ChangePassword(password string) error {
+	errCompare := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	if errCompare == nil {
+		return ErrSamePassword
+	}
+	err := u.setPassword(password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ReactivateToken resets the activation token in the case the user did not activate the account
+// in time. Returns an activationToken
+func (u *User) ReactivateToken() (string, error) {
+	newToken := uuid.New().String()
+	u.ActivationToken = newToken
+	return newToken, nil
+}
+
+// Activate user. You would need to provide a activation token to check if it correct.
+// If correct, it would return the status of the user which should be true or false
+func (u *User) Activate(activationToken string) (bool, error) {
+	if u.ActivationToken == activationToken {
+		u.Activated = true
+		return true, nil
+	}
+	return false, ErrActivationTokenInvalid
 }
