6484b1c17e1331ed363d124d8648bb8178c9b1be
Hairizuan <hairizuanbinnoorazman@gmail.com>
2021-08-09 07:57:32 +0800

    added new setters - copied style from project module

--- user/store.go
+++ user/store.go
@@ -1,9 +1,52 @@
 package user
 
-import "context"
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Store interface {
 	StoreUser(ctx context.Context, u User) error
 	GetUser(ctx context.Context, ID string) (User, error)
 	GetUserByEmail(ctx context.Context, Email string) (User, error)
+}
+
+func setForgetPasswordToken() func(*User) error {
+	return func(a *User) error {
+		a.ForgetPasswordToken = uuid.New().String()
+		a.ForgetPasswordExpiryDate = time.Now().Add(1 * time.Hour)
+		return nil
+	}
+}
+
+// This function is needed in the case we need to resend activation link once more
+func setActivationToken() func(*User) error {
+	return func(a *User) error {
+		a.ActivationToken = uuid.New().String()
+		a.ActivationExpiryDate = time.Now().Add(1 * time.Hour)
+		return nil
+	}
+}
+
+func setNewPassword(password string) func(*User) error {
+	return func(a *User) error {
+		a.ForgetPasswordToken = ""
+		a.ForgetPasswordExpiryDate = time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
+		err := a.setPassword(password)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func setActivate() func(*User) error {
+	return func(a *User) error {
+		a.ActivationToken = ""
+		a.ActivationExpiryDate = time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
+		a.Activated = true
+		return nil
+	}
 }
